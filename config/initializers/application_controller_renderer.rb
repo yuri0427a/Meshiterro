2f37adb238758a118d0c701bc95b314ce46ce6dc
yuri0427a <yuri0427ita@gmail.com>
2020-01-25 06:55:40 +0000

    [Finish]5-3

--- config/initializers/application_controller_renderer.rb
+++ config/initializers/application_controller_renderer.rb
@@ -6,3 +6,4 @@
 #     https: false
 #   )
 # end
+Refile.secret_key = 'cea5bf0022eaf7eb94fb4a4879f2f18ecad18b6c6d0e2b67be40c296edb3031f3918194eb6ecf7a7bc1218e7cdfbaaca582d029808fdc7f18f1746dff29bf212'
